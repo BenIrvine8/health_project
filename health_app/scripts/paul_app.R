d4c6bdcd3a68295e40aae3b6d7b43b2a59f8e437
pjbateman <84747827+pjbateman@users.noreply.github.com>
2021-08-10 16:42:59 +0100

    afternoon update

--- health_app/scripts/paul_app.R
+++ health_app/scripts/paul_app.R
@@ -8,20 +8,16 @@ life <- read_csv(here("data/clean_data/life_expectancy_clean.csv"))
 # reformatting and sorting the age categories
 life <- life %>% 
   mutate(age_new = str_remove(age, " years"))
-date_ranges <- distinct(.data = life, date_code) 
-simd_codes <- distinct(.data = life, simd_quintiles)
+#simd_codes <- distinct(.data = life, simd_quintiles)
+simd_codes <- c("All", "5 - least deprived", "4", "3", "2", "1 - most deprived")
 
 # -------------------------------------------------------------------------
 
 
 ui <- fluidPage(
-  theme = shinytheme("slate"),
   titlePanel(tags$h1("Life Expectancy at Birth")),
   fluidRow(
     column(3, 
-           selectInput("date_code",
-                       "Which date range?",
-                       choices = date_ranges),
            selectInput("simd_quintiles",
                        "Which SIMD quintile?",
                        choices = simd_codes)
@@ -29,6 +25,8 @@ ui <- fluidPage(
     column(9,
            plotOutput("birth_plot"))
   ),
+  br(),
+  br(),
   fluidRow(
     column(3),
     column(9,
@@ -50,19 +48,23 @@ server <- function(input, output) {
     # life expectancy at zero years
     life %>% 
       filter(age == "0 years",
-             date_code == input$date_code,
+             date_code == "2016-2018",
              simd_quintiles == input$simd_quintiles,
              urban_rural_classification == "All",
              str_detect(area_code, "^S92")) %>% 
       ggplot() +
-      aes(x = sex, y = years_to_live) +
+      aes(x = sex, y = years_to_live, fill = sex) +
       geom_col() +
       scale_y_continuous() +
       labs(
         x = "\nSex",
-        y = "Life expectancy (years) at birth",
-        title = "Life expectancy in Scotland\n",
-        subtitle = "Data from the Scottish Government\n")
+        y = "Life expectancy (years)",
+        title = "Life expectancy in Scotland at birth in 2016-2018\n",
+        subtitle = "(years) Data from the Scottish Government\n") +
+      theme_wsj() +
+      scale_fill_wsj(palette = "rgby") +
+      expand_limits(y = c(1,100)) +
+      geom_text(aes(label = years_to_live), vjust = -0.5)
   })
   
   output$gender_plot <- renderPlot({
@@ -72,7 +74,7 @@ server <- function(input, output) {
     
     life %>% 
       filter(sex != "All",
-             date_code == input$date_code,
+             date_code == "2016-2018",
              simd_quintiles == input$simd_quintiles,
              urban_rural_classification == "All",
              str_detect(area_code, "^S92")) %>% 
@@ -83,8 +85,11 @@ server <- function(input, output) {
       labs(
         x = "\nage (years)",
         y = "years to live",
-        title = "Life expectancy in Scotland\n",
-        subtitle = "Data from the Scottish Government\n")
+        title = "Life expectancy in Scotland in 2016-2018\n",
+        subtitle = "(years, by age group) Data from the Scottish Government\n") +
+      theme_wsj() +
+      scale_fill_wsj(palette = "rgby") +
+      expand_limits(y = c(1,100)) 
     
   })
   
