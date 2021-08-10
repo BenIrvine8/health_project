c0501070cabd44f67abcab785be96021c222375e
barrodea <85226393+barrodea@users.noreply.github.com>
2021-08-10 10:43:42 +0100

    adds survey tab to shiny app

---
+++ health_app/global.R
@@ -0,0 +1,9 @@
+
+# read in packages
+library(tidyverse)
+library(janitor)
+library(here)
+
+# read in survey data
+scottish_survey <- read.csv(here("data/clean_data/scotland_health_survey_clean.csv"))
+scottish_survey_local <- read_csv(here("data/clean_data/scotland_health_survey_local_clean.csv"))
--- health_app/ui.R
+++ health_app/ui.R
@@ -7,11 +7,40 @@ library(shiny)
 
 # Define UI for health application
 shinyUI(navbarPage("Scottish Public Health",
-          tabPanel("Overview"),
+          tabPanel("Scottish Health Survey Overview"),
+          fluidRow(
+            column(3, selectInput("gender_input",
+                                  "Select gender",
+                                  choices = unique(scottish_survey$sex)),
+                   
+                   
+                   selectInput("indicator_input",
+                               "Select indicator",
+                               choices = unique(scottish_survey$scottish_health_survey_indicator))
+            ),
+            
+            column(9, plotOutput("trendPlot",width = "800"))
+            
+          ),
+          mainPanel(
+            fluidRow(
+              column(4, selectInput("sex_input",
+                                    "Select gender",
+                                    choices = unique(scottish_survey_local$sex)),
+                     
+                     selectInput("indic_input",
+                                 "Select indicator",
+                                 choices = unique(scottish_survey_local$scottish_health_survey_indicator))),
+              
+              column(8, plotOutput("localPlot", width = "800")),
+            )
+          ),
+          
           tabPanel("Focus"),
           tabPanel("SMID"),
           tabPanel("Download")
                    )
-        )
+)
+
 
 
